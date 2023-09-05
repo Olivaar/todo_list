d9121adb8d97772444fd40177d732e71ff1fb11a
Olivaar <mocolivarbaloi@gmail.com>
2023-09-04 21:58:08 -0300

    Implements Slideble class in todp_list_item.dart

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import '../models/tarefa.dart';
@@ -13,33 +14,45 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.grey[200],
-      ),
-      padding: EdgeInsets.all(8),
-      margin: EdgeInsets.symmetric(vertical: 2),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      endActionPane: const ActionPane(
+        motion: StretchMotion(),
         children: [
-          Text(
-            DateFormat('dd/MM/yyy - HH:mm').format(tarefa.date),
-            style: TextStyle(
-              fontSize: 12
-            ),
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Excluir',
           ),
-          Text(
-            tarefa.title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+        ]
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.grey[200],
+        ),
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.symmetric(vertical: 2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch ,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyy - HH:mm').format(tarefa.date),
+              style: TextStyle(
+                fontSize: 12
+              ),
             ),
-          ),
-        ],
+            Text(
+              tarefa.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-//TODO Implementar Slidable
